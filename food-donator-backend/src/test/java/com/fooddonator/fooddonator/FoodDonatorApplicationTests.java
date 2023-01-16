266e7f476f1f137360fe7f4b98f23b5fec0cfa54
dylan2894 <28scda94@gmail.com>
2023-01-16 08:19:51 +0200

    init

---
+++ food-donator-backend/src/test/java/com/fooddonator/fooddonator/FoodDonatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fooddonator.fooddonator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDonatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
