6b81d23b96347040f48328d68d8b0419b36bc81b
dylan2894 <28scda94@gmail.com>
2023-01-25 00:07:18 +0200

    ✨(client): Added Receiver's Google Map

---
+++ food-donator-frontend/src/app/components/receiver-map/receiver-map.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-receiver-map',
+  templateUrl: './receiver-map.component.html',
+  styleUrls: ['./receiver-map.component.css']
+})
+export class ReceiverMapComponent {
+
+}
