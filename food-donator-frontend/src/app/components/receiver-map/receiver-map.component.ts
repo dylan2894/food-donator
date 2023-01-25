e746485f31b6da5a879b842f2820afe97dd4f9d0
dylan2894 <28scda94@gmail.com>
2023-01-25 21:42:01 +0200

    ➕Added jQuery, cssnext and postcss

--- food-donator-frontend/src/app/components/receiver-map/receiver-map.component.ts
+++ food-donator-frontend/src/app/components/receiver-map/receiver-map.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any
 
 @Component({
   selector: 'app-receiver-map',
   templateUrl: './receiver-map.component.html',
   styleUrls: ['./receiver-map.component.css']
 })
-export class ReceiverMapComponent {
+export class ReceiverMapComponent implements OnInit {
 
+  isJqueryWorking = "Not working";
+
+  ngOnInit() {
+    $(document).ready(function(){
+      $('select').formSelect();
+
+      $('select').on('change', function(this: any) {
+        console.log(this.value);
+      });
+    });
+  }
+
+  mapOptions: google.maps.MapOptions = {
+    center: { lat: -25.781951024040037, lng: 28.338064949199595 },
+    zoom: 16,
+    zoomControl: true,
+    mapTypeControl: false,
+    streetViewControl: true,
+    fullscreenControl: false
+  };
 }
