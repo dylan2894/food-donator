142df4029056672613c38f06e531db932820cefb
Dylan Pietersen <dpietersen@retrorabbit.co.za>
2023-01-28 22:48:02 +0200

    🚧(client) Working on Donor Dashboard UI

--- food-donator-frontend/src/app/app-routing.module.ts
+++ food-donator-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DonorDashboardComponent } from './components/donor-dashboard/donor-dashboard.component';
 import { ReceiverMapComponent } from './components/receiver-map/receiver-map.component';
 
 const routes: Routes = [
+  { path: 'donor-dashboard', component: DonorDashboardComponent },
   { path: '**', component: ReceiverMapComponent }
 ];
 
