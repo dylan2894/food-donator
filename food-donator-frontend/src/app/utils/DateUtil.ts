6648931340ce99d8b7b47ed54c8a4e9533498344
Dylan Pietersen <dpietersen@retrorabbit.co.za>
2023-04-05 12:58:21 +0200

    Fixing epoch date being one day too early

--- food-donator-frontend/src/app/utils/DateUtil.ts
+++ food-donator-frontend/src/app/utils/DateUtil.ts
@@ -1,6 +1,6 @@
 export default class DateUtil {
   epochDateToReadable(epochDate: number): string {
-    const date = new Date(epochDate);
+    const date = new Date(epochDate + 86400000); // plus one day in ms to match timezone (GMT+2)
     return date.toISOString().split('T')[0];
   }
 }
