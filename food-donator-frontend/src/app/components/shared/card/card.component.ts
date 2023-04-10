e964674a7c350991cc26bb9f52b3ad9d39d2a891
Dylan Pietersen <dpietersen@retrorabbit.co.za>
2023-04-10 23:19:03 +0200

    Adding donor name to donations cards

--- food-donator-frontend/src/app/components/shared/card/card.component.ts
+++ food-donator-frontend/src/app/components/shared/card/card.component.ts
@@ -1,11 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  @Input() donorId = '';
   @Input() date = '';
   @Input() startTime = '';
   @Input() endTime = '';
@@ -14,9 +17,21 @@ export class CardComponent {
   @Input() isCurrent = false;
   @Input() forCarousel = false;
 
-  constructor() {
-    $(document).ready(() => {
+  correspondingDonor: User|null = null;
+
+  constructor(private userService: UserService) {
+    $(() => {
+      // initialize chips UI component
       $('.chips').chips();
     });
+  }
+
+  ngOnInit() {
+    // if donorId is supplied, fetch the donor which corresponds to this donation card.
+    if(this.donorId != '') {
+      this.userService.getUser(this.donorId).then((user) => {
+        if(user != null){ this.correspondingDonor = user ; }
+      });
+    }
   }
 }
