fd9e6f83d6d722768457e289e70841e79f6c7619
Dylan Pietersen <dpietersen@retrorabbit.co.za>
2023-04-12 16:15:48 +0200

    Parameterising chip-selector for Tag types

--- food-donator-frontend/src/app/components/shared/chip-selector/chip-selector.component.ts
+++ food-donator-frontend/src/app/components/shared/chip-selector/chip-selector.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Tag } from 'src/app/models/tag.model';
+import { TagService } from 'src/app/services/tag/tag.service';
 
 @Component({
   selector: 'app-chip-selector',
@@ -6,34 +8,36 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./chip-selector.component.css']
 })
 export class ChipSelectorComponent {
-  chips: string[] = ["Canned Food", "Grains", "Long Life Milk", "Blankets", "Clothes","Clothes","Clothes","Clothes","Clothes","Clothes"];
-  selectedArray: string[] = [];
-  @Output() selectedEvent = new EventEmitter<string[]>();
+  //chips: string[] = ["Canned Food", "Grains", "Long Life Milk", "Blankets", "Clothes","Clothes","Clothes","Clothes","Clothes","Clothes"];
+  chips: Tag[] = [];
+  selectedArray: Tag[] = [];
+  @Output() selectedEvent = new EventEmitter<Tag[]>();
 
-  constructor() {
-    //TODO fetch tags from Tag table
-    this.tagService.getTags() {
-
-    }
+  constructor(private tagService: TagService) {
+    this.tagService.getTags().then((tags) => {
+      if(tags != null) {
+        this.chips = tags;
+      }
+    });
   }
 
-  toggleChipSelect(chip: string) {
+  toggleChipSelect(chip: Tag) {
     if(this.selectedArray.includes(chip)) {
       this.selectedArray = this.removeSelection(chip);
-      $('.chip:contains("'+chip+'")').removeClass("selected");
-      $('.chip:contains("'+chip+'")').html(chip);
+      $('.chip:contains("'+chip.name+'")').removeClass("selected");
+      $('.chip:contains("'+chip.name+'")').html(chip.name);
 
       this.selectedEvent.emit(this.selectedArray);
       return;
     }
 
     this.selectedArray.push(chip);
-    $('.chip:contains("'+chip+'")').addClass("selected");
-    $('.chip:contains("'+chip+'")').html(chip + "<i class='close material-icons'>close</i>");
+    $('.chip:contains("'+chip.name+'")').addClass("selected");
+    $('.chip:contains("'+chip.name+'")').html(chip.name + "<i class='close material-icons'>close</i>");
     this.selectedEvent.emit(this.selectedArray);
   }
 
-  private removeSelection(chip: string) {
-    return this.selectedArray.filter(selection => selection != chip);
+  private removeSelection(chip: Tag) {
+    return this.selectedArray.filter(selection => selection.name != chip.name);
   }
 }
