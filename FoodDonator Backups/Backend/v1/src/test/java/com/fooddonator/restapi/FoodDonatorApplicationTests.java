57acb8ace2c15087898e322c106d3c774f936ec5
Dylan Pietersen <dpietersen@retrorabbit.co.za>
2023-06-19 20:21:44 +0200

    Adding backup files

---
+++ FoodDonator Backups/Backend/v1/src/test/java/com/fooddonator/restapi/FoodDonatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fooddonator.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDonatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
